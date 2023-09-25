5353488f0ba8da2a16d4eebc36d6f1500b1a35e8
Alireza Aghaei <appleiran66@gmail.com>
2023-09-25 17:28:14 +0330

    Bosch landing finished

--- landing/Bosch/assets/js/script.js
+++ landing/Bosch/assets/js/script.js
@@ -1,43 +1,124 @@
-$(document).ready(function(){
- $('.owl-carousel').owlCarousel({
-     rtl:true,
-     loop:true,
-     dots: false,
-     nav: false,
-     autoplay: true,
-     autoplayHoverPause: true,
-     singleItem:true,
-     item: 1,
-     autoplayTimeout: 4000,
-     responsive:{
-        0:{
-            items:1,
-            margin: 10
+var nameValid = false;
+var locationValid = false;
+var phoneValid = false;
+var serviceValid = false;
+
+var nameInput = '';
+var locationInput = '';
+var phoneInput = '';
+var serviceInput = '';
+
+function sendAjaxForm() {
+    // $('form').submit(function () {
+    //     return false;
+    // });
+    var name, location, phone, service;
+    var a = 'ld';
+    name = nameInput;
+    service = serviceInput;
+    phone = phoneInput;
+    location = locationInput;
+
+    $('#send_button').addClass('pending');
+    $('#send_button').prop('disabled', true);
+
+    $.ajax({
+        url: 'form.php',
+        method: "POST",
+        data: {
+            'ajax': 'true',
+            'in': '5' + a,
+            'name': name,
+            'phone': phone,
+            'service': service,
+            'location': location
+        },
+        success: function (result) {
+            $('#send_button').removeClass('pending');
+            // elm.addClass('success');
+            $('.successDiv').addClass("open");
+            $('#sec_6 form').hide();
         }
-       }
- });
+    });
+
+}
+
+
+$('form').submit(function () {
+    return false;
+});
+// insert data cover form 
+
+  $("#name_input").blur(function () {
+    if ($(this).is(":invalid") || this.value.length < 2) {
+      nameValid = false;
+    } else {
+      nameInput = $(this).val();
+      nameValid = true;
+    };
+  });
+  $("#phone_input").blur(function () {
+    if ($(this).is(":invalid") || this.value.length <= 10) {
+      phoneValid = false;
+    } else {
+      phoneInput = $(this).val();
+      phoneValid = true;
+    };
+  });
+  $("#ort_input").blur(function () {
+    if ($(this).is(":invalid") || !$(this).val()) {
+      locationValid = false;
+    } else {
+      locationInput = $(this).val();
+      locationValid = true;
+    };
+  });
+
+
+$('#send_button').click(function () {
+    if ($('#select input').is(":checked")) {
+        serviceInput = $('input[name="service"]:checked').val();
+    }
+    if (phoneValid){
+        sendAjaxForm();
+    }else {
+        alert('لطفا شماره تماس خود را به طور صحیح وارد کنید.')
+        return false;
+    }
 });
 
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     nav:false,
-//     dots:false,
-//     rtl:true,
-//     autoplay: true,
-//     autoplayTimeout: 3000,
-//     margin: 20,
-//     // navText : ["<img src='assets/images/right_button.svg' width='54px'>","<img src='assets/images/left_button.svg' width='54px'>"],
-//     responsive:{
-//       0:{
-//           items:1,
-//           margin: 10
-//       },
-//       600:{
-//           items:3,
-//           margin: 10,
-//       },
-//       1000:{
-//           items:3,
-//       }
-//      }
-// });
+
+
+
+
+
+
+
+
+
+
+
+// just get 11 number input
+var alireza = document.querySelectorAll(".alireza_validator"),i;
+// Restricts input for the given textbox to the given inputFilter.
+function setInputFilter(textbox, inputFilter) {
+  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
+        textbox.addEventListener(event, function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            }
+        });
+    });
+}
+for (i = 0; i < alireza.length; ++i) {
+    alireza[i].maxLength = 11;
+    // Restrict input to digits and '.' by using a regular expression filter.
+    setInputFilter(alireza[i], function (value) {
+        return /^[0-9۰-۹]*$/i.test(value);
+    });
+}
---
+++ landing/Bosch/form.php
@@ -0,0 +1,68 @@
+<?php
+ini_set("soap.wsdl_cache_enabled", "0");
+error_reporting(0);
+if($_POST['ajax']=='true'){
+//include_once('../../db_config.php');
+
+$browser = $_SERVER['HTTP_USER_AGENT'];
+if(!empty($_SERVER['HTTP_CLIENT_IP'])){$ip = $_SERVER['HTTP_CLIENT_IP'];}elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];}else{$ip = $_SERVER['REMOTE_ADDR'];}
+
+// database details
+    $host = "localhost";
+    $username = "adverlin_landing";
+    $password = "T2y9k)8(Qr2bJW";
+    $dbname = "adverlin_landing";
+
+    // creating a connection
+    $db_connection = mysqli_connect($host, $username, $password, $dbname);
+
+    // to ensure that the connection is made
+    if (!$db_connection)
+    {
+        die("Connection failed!" . mysqli_connect_error());
+    }
+    
+    mysqli_set_charset($db_connection,'utf8');
+    date_default_timezone_set('Asia/Tehran');
+    $datetime=date('Y-m-d H:i:s');
+
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $service = $_POST['service'];
+    $location = $_POST['location'];
+
+    mysqli_query($db_connection,"INSERT INTO Newpackage (name, phone , location, service, date) VALUES ('$name', '$phone', '$service','$location' ,'$datetime')");
+
+
+//sms config
+$smsconfigsql=mysqli_query($db_connection,"SELECT * FROM `variable` WHERE `type`='sms'");
+while($smsconfigrow=mysqli_fetch_assoc($smsconfigsql)){$smsconfig[$smsconfigrow['name']]=$smsconfigrow['value'];}
+             
+//$message2Send='شماره جدید : '.$phone.'';
+//$url='https://digital-lead.ir/engine/novin/'.urlencode($message2Send).'/false';
+//$url=rtrim($url,'/');
+//echo file_get_contents($url);
+function sendMessage($smsuser,$smspass,$smsfrom,$smsphone,$smstext){
+$sms_client = new SoapClient('http://api.payamak-panel.com/post/send.asmx?wsdl', array('encoding'=>'UTF-8'));
+$parameters['username'] = $smsuser;
+$parameters['password'] = $smspass;
+$parameters['to'] = $smsphone;
+$parameters['from'] = $smsfrom;
+$parameters['text'] =$smstext;
+$parameters['isflash'] =false;
+echo $sms_client->SendSimpleSMS2($parameters)->SendSimpleSMS2Result;
+}
+function smscompiler($string){
+global $service,$phone,$location,$name,$datetime;
+$string=str_replace('{service}',$service,$string);
+$string=str_replace('{phone}',$phone,$string);
+$string=str_replace('{location}',$location,$string);
+$string=str_replace('{name}',$name,$string);
+$string=str_replace('{datetime}',$datetime,$string);
+return $string;
+}
+    
+sendMessage($smsconfig['smsuser'],$smsconfig['smspass'],$smsconfig['smsfrom'],$smsconfig['smsadminphone'],smscompiler($smsconfig['smsadmintext']));
+// sendMessage($smsconfig['smsuser'],$smsconfig['smspass'],$smsconfig['smsfrom'],$phone,smscompiler($smsconfig['smstext']));    
+}
+?>
